36422800120710453ffe74b23a6cf48bb4ede349
YuryShageev <schageev.yur@yandex.ru>
2023-02-20 16:07:32 +0300

    Continue to the next Homework

--- src/main/java/recipeolga/Controllers/RecipeController.java
+++ src/main/java/recipeolga/Controllers/RecipeController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import recipeolga.Model.Recipe;
 import recipeolga.Services.RecipeService;
 
