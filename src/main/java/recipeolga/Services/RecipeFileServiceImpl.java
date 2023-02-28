6316e1d50cd878411e6b90401520b7a7bd84dce5
YuryShageev <schageev.yur@yandex.ru>
2023-02-28 14:32:48 +0300

    Continue to the next Homework

---
+++ src/main/java/recipeolga/Services/RecipeFileServiceImpl.java
@@ -0,0 +1,61 @@
+package recipeolga.Services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import recipeolga.exception.FileProcessingException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service("recipeFileService")
+public class RecipeFileServiceImpl implements FileService{
+
+    @Value("src/main/resources")
+    private String dataFilePathIngredient;
+    @Value("recipe.json")
+    private String dataFileNameRecipe;
+
+    @Override
+    public boolean saveToFile(String json) {
+        try {
+            cleanDataFile();
+            Files.writeString(Path.of(dataFilePathIngredient, dataFileNameRecipe), json);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String readFromFile() {
+        if (Files.exists(Path.of(dataFilePathIngredient, dataFileNameRecipe))) {
+            try {
+                return Files.readString(Path.of(dataFilePathIngredient, dataFileNameRecipe));
+            } catch (IOException e) {
+                throw new FileProcessingException("Не удалось прочитать файл");
+            }
+        } else {
+            return "{}";
+        }
+    }
+
+    @Override
+    public boolean cleanDataFile() {
+        try {
+            Path path = Path.of(dataFilePathIngredient, dataFileNameRecipe);
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public File getDataFileTxt() {
+        return new File(dataFilePathIngredient + "/" + dataFileNameRecipe);
+    }
+}
